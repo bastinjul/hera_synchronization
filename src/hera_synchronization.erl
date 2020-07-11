ae552095ee9b38b19e3884c65c78ac8d422060e7
Julien Bastin <julien.bastin@student.uclouvain.be>
2020-07-11 23:57:22 +0200

    update start policy

--- src/hera_synchronization.erl
+++ src/hera_synchronization.erl
@@ -9,6 +9,10 @@
 
 -export([start/2, stop/1]).
 
+start({takeover, _Node}, _StartArgs) ->
+    Measurements = application:get_env(hera_synchronization, measurements),
+    [hera:maybe_propagate(fun() -> hera:restart_measurement(Meas, true) end) || {Meas, _} <- Measurements],
+    hera_synchronization_sup:start_link();
 start(_StartType, _StartArgs) ->
     hera_synchronization_sup:start_link().
 
