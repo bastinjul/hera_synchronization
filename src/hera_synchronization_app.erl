91a1cc4a618f036a447b2353a27a34abf3b51cde
Julien Bastin <julien.bastin@student.uclouvain.be>
2020-07-13 15:45:11 +0200

    Correct start function

--- src/hera_synchronization_app.erl
+++ src/hera_synchronization_app.erl
@@ -9,8 +9,9 @@
 
 -export([start/2, stop/1]).
 
-start({takeover, _Node}, _StartArgs) ->
-    Measurements = application:get_env(hera_synchronization, measurements),
+start(StartType, _StartArgs) when StartType =/= normal ->
+    logger:notice("hera_synchronization restarted on other node with type : ~p~n", [StartType]),
+    {ok, Measurements} = application:get_env(hera_synchronization, measurements),
     [hera:maybe_propagate(fun() -> hera:restart_measurement(Meas, true) end) || {Meas, _} <- Measurements],
     hera_synchronization_sup:start_link();
 start(_StartType, _StartArgs) ->
